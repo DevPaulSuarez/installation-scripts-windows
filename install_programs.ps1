7b00b222ece630d64f1efa2afc4a73bdad1fa1b9
DevPaulSuarez <paulSuarez018@gmail.com>
2025-04-22 20:55:33 -0600

    Agregar script de instalación con winget para windows

---
+++ install_programs.ps1
@@ -0,0 +1,38 @@
+# Actualizar Winget (opcional)
+winget upgrade --all
+
+# Instalar Git
+winget install Git.Git
+
+# Instalar Visual Studio Code
+winget install Microsoft.VisualStudioCode
+
+# Instalar Node.js
+winget install OpenJS.NodeJS.LTS
+
+# Instalar Python
+winget install Python.Python.3.13
+winget install Python.Python.3.12
+
+# Instalar JDK (Java Development Kit)
+winget install Oracle.JDK.17
+
+# Instalar Microsoft Visual C++ Redistributable
+winget install Microsoft.VCRedist.2010.x64
+winget install Microsoft.VCRedist.2015+.x64
+
+# Instalar Postman
+winget install Postman.Postman
+
+# Instalar Composer (en caso de que sea necesario)
+# Asegúrate de que el identificador sea correcto
+winget install ARP\Machine\X86\{7315AF68-E777-496AΓÇª
+
+# Instalar Google Chrome
+winget install Google.Chrome.EXE
+
+# Instalar Visual Studio Installer
+winget install Microsoft.VisualStudio.Installer
+
+# Instalar Oracle VirtualBox (corregido)
+winget install Oracle.VirtualBox
